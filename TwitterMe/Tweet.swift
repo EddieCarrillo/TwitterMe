fd536e1aa87a07bd2d5fc00e73cd0075ccc385bf
EddieCurio <edcarril@ucsd.edu>
2017-11-01 09:56:31 -0700

    Created Model files

---
+++ TwitterMe/Tweet.swift
@@ -0,0 +1,13 @@
+    //
+//  Tweet.swift
+//  TwitterMe
+//
+//  Created by my mac on 11/1/17.
+//  Copyright © 2017 ecproductions. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
---
+++ TwitterMe/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  TwitterMe
+//
+//  Created by my mac on 11/1/17.
+//  Copyright © 2017 ecproductions. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+    var name: String?
+    var screenname: String?
+    var profileUrl: URL?
+    var tagline: String?
+
+}
