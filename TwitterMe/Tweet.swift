f24f6f5fbe425c0a356225af4b8601899185e8a8
EddieCurio <edcarril@ucsd.edu>
2017-11-02 09:39:29 -0700

    Created JSON Deserialization code for tweets and user models

--- TwitterMe/Tweet.swift
+++ TwitterMe/Tweet.swift
@@ -9,5 +9,41 @@
 import UIKit
 
 class Tweet: NSObject {
+    
+    var text: String?
+    var timeStamp: NSDate?
+    var retweetCount: Int = 0
+    var favoritesCount: Int = 0
+    
+    
+    
+    init(dictionary: NSDictionary){
+        self.text = dictionary["text"] as? String
+        self.retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
+        self.favoritesCount = (dictionary["favorites_count"] as? Int) ?? 0
+        
+        if  let timestampString = dictionary["time_stamp"] as? String{
+            let formatter = DateFormatter()
+            
+            formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+            
+           self.timeStamp = formatter.date(from: timestampString)
+        }
+      
+        
+    }
+    
+    
+    class func tweetsWithArray(dictionaries: [NSDictionary]) -> [Tweet] {
+        var tweets: [Tweet] = []
+        
+        
+        for dictionary in dictionaries {
+            let tweet = Tweet(dictionary: dictionary)
+            
+            tweets.append(tweet)
+        }
+        
+    }
 
 }
--- TwitterMe/User.swift
+++ TwitterMe/User.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
+//Models used also for persistence
+
 class User: NSObject {
     
     var name: String?
     var screenname: String?
     var profileUrl: URL?
     var tagline: String?
+    
+    
+    init(dictionary: NSDictionary){
+        
+        //Deserialization code
+        self.name = dictionary["name"] as? String
+        
+        self.screenname = dictionary["screen_name"] as? String
+        
+        if   let profileUrlString = dictionary["profile_image_url_https"] as? String {
+            self.profileUrl = URL(string: profileUrlString)
+        }
+        
+        
+        self.tagline = dictionary["description"] as? String
+        
+    }
 
 }
