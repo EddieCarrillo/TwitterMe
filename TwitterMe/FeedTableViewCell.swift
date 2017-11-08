979ddcaddba053f6328bb4427f5b55a42e3bd53d
EddieCurio <edcarril@ucsd.edu>
2017-11-07 17:35:43 -0800

    Added pods

---
+++ TwitterMe/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  TwitterMe
+//
+//  Created by my mac on 11/7/17.
+//  Copyright © 2017 ecproductions. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
