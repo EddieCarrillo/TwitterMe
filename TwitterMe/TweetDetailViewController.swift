812e7c0904922954f7fe096cf7408629385afa07
Eduardo Carrillo <edcarril@its-sungod-mac5.ucsd.edu>
2017-11-29 15:47:32 -0800

    Created TweetDetailViewController file.

---
+++ TwitterMe/TweetDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TweetDetailViewController.swift
+//  TwitterMe
+//
+//  Created by Eduardo Carrillo on 11/29/17.
+//  Copyright © 2017 ecproductions. All rights reserved.
+//
+
+import UIKit
+
+class TweetDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
