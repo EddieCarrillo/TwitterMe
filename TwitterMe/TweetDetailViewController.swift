7b035be214093e88b86ccb2f12f841801ebc7cca
Eduardo Carrillo <edcarril@its-sungod-mac5.ucsd.edu>
2017-11-29 16:42:18 -0800

    Added date formatter code. Added code to update the GUI.

--- TwitterMe/TweetDetailViewController.swift
+++ TwitterMe/TweetDetailViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class TweetDetailViewController: UIViewController {
 
+    
+    @IBOutlet weak var profilePictureImageView: UIImageView! //Done
+   
+    @IBOutlet weak var profileNameLabel: UILabel! // Done
+    
+    @IBOutlet weak var handleNameLabel: UILabel! //Done
+    
+    @IBOutlet weak var tweetTextLabel: UILabel! //Done
+    
+    @IBOutlet weak var mediaView: UIView! //TODO
+    
+    @IBOutlet weak var createdAtLabel: UILabel! //Done
+    
+    @IBOutlet weak var retweetNumberLabel: UILabel! //Done
+    
+    @IBOutlet weak var favoritesNumberLabel: UILabel! //Done
+    
+    
+    var tweet: Tweet? {
+        didSet{
+            guard let tweet = self.tweet else {
+               print("User assigned tweet as nil value. [WEIRD BUG]")
+            
+            }
+            
+            if let tweetOwner = tweet.owner {
+                if let profilePictureUrl = tweetOwner.profileUrl {
+                    self.profilePictureImageView.setImageWith(profilePictureUrl)
+                }else {
+                    print("ERROR USING URL")
+                }
+               
+            }else {
+               print("ERROR LOADING USER!")
+            }
+            
+             self.profileNameLabel.text = tweet.ownerName
+             self.tweetTextLabel = tweet.text
+            
+            self.createdAtLabel = getFormattedDateString()
+            
+             self.retweetNumberLabel.text =  "\(tweet.retweetCount)"
+             self.favoritesNumberLabel.text = "\(tweet.favortiesCount)"
+            self.handleNameLabel.text = "\(tweet.owner.screenname)"
+  
+        }
+        
+        
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +71,44 @@ class TweetDetailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func getFormattedDateString() -> String {
+        let dateStringUnformatted = tweet.dateText
+        let dateStringFormatUnformatted =  "EEE MMM d HH:mm:ss Z y"
+        //Weekday Month day Hour(Military) +0000 Year
+        
+        let dateFormatterUnformatted = DateFormatter()
+        dateFormatterUnformatted.dateFormat = dateStringFormatUnformatted
+        
+        let date = dateFormatterUnformatted.date(from: dateStringUnformatted)
+        
+        let dateFormatter = DateFormatter()
+        //Formatter string text for time
+        let timeFormatterString = "h:mm a"
+        
+        timeFormatterString.amSymbol = "AM"
+        timeFormatterString.pmSymbol = "PM"
+        
+        dateFormatter.dateFormat = dateFormatterString
+        //Actual time string formatted correctly
+        let timeDateString = dateFormatter.string(from: date)
+        
+        
+        //Formatter string text for date created
+        let dateFormatterString = "dd MMM YY"
+        
+        dateFormatter.dateFormat = dateFormatterString
+        
+        let dateString = dateFormatter.string(from: date)
+        
+        let completeDateString = "\(timeDateString) · \(dateString)"
+    
+        
+        return completeDateString
+    
+    
     }
     
 
