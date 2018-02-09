b1bf4ea58e470763775d0452d16f1b8756d13aff
Eduardo Carrillo <edcarril@ucsd.edu>
2018-02-08 16:01:50 -0800

    Added constraints in new general cell.

---
+++ TwitterMe/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  TwitterMe
+//
+//  Created by Eduardo Carrillo on 2/8/18.
+//  Copyright © 2018 ecproductions. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
